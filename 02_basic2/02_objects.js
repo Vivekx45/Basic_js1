5852c1ba37360184075b349e9bf5404a1bf5a964
Vivekx45 <vivektetwal129@gmail.com>
2025-12-18 15:04:14 +0000

    Objects

---
+++ 02_basic2/02_objects.js
@@ -0,0 +1,23 @@
+//Objects
+/*
+objects can be declare of 2 types by literals, constructors
+-there is a concept "singleton" it means when you make object with constuctor then it becomes
+ singleton object
+-in the case of literals declaration there is no singleton object will create its multiple instaces
+ will create automatically
+*/
+
+// Object.create we can also declare object like this, this is the constructor method
+
+
+//Object literals
+
+const JsUser = {
+    name: "Vivek",
+    age: 20,
+    location: "Mumbai",
+    email: "vivek",
+    isloggedIn: false,
+    arr: ["x", "y", "z"],
+
+}
