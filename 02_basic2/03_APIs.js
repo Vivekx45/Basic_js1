919060396423371548c3418697729487b6e6f3f9
Vivekx45 <vivektetwal129@gmail.com>
2025-12-21 11:43:57 +0000

    JSON APIs

--- 02_basic2/03_APIs.js
+++ 02_basic2/03_APIs.js
@@ -23,70 +23,71 @@ Before technology isn't advanced the data you got into XML file now you got data
 
 //to understand these shown below APIs we have to use some tools 
 //like JSON formatter
-{
-  "results": [
-    {
-      "gender": "male",
-      "name": {
-        "title": "Mr",
-        "first": "Elias",
-        "last": "Christiansen"
-      },
-      "location": {
-        "street": {
-          "number": 3006,
-          "name": "Dalvej"
-        },
-        "city": "Sundby",
-        "state": "Syddanmark",
-        "country": "Denmark",
-        "postcode": 23799,
-        "coordinates": {
-          "latitude": "-44.6656",
-          "longitude": "-131.3606"
-        },
-        "timezone": {
-          "offset": "-2:00",
-          "description": "Mid-Atlantic"
-        }
-      },
-      "email": "elias.christiansen@example.com",
-      "login": {
-        "uuid": "41541dd2-518e-49a9-987f-c687cde0223c",
-        "username": "heavywolf823",
-        "password": "lenny",
-        "salt": "weI4KOJM",
-        "md5": "3d582144fd283da5b32a2346a9e49c44",
-        "sha1": "e9fca9e27a3dd5430980a72e2226d0e6ab629a34",
-        "sha256": "185190a988586a37e2f741dfcca9f30cd0b1e259da3c8eb21480c7732b2767d7"
-      },
-      "dob": {
-        "date": "1969-02-03T03:49:24.651Z",
-        "age": 56
-      },
-      "registered": {
-        "date": "2012-12-20T15:12:23.146Z",
-        "age": 13
-      },
-      "phone": "78901127",
-      "cell": "63801297",
-      "id": {
-        "name": "CPR",
-        "value": "020269-9909"
-      },
-      "picture": {
-        "large": "https://randomuser.me/api/portraits/men/45.jpg",
-        "medium": "https://randomuser.me/api/portraits/med/men/45.jpg",
-        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/45.jpg"
-      },
-      "nat": "DK"
-    }
-  ],
-  "info": {
-    "seed": "397100682af9e782",
-    "results": 1,
-    "page": 1,
-    "version": "1.4"
-  }
-}
+
+// {
+//   "results": [
+//     {
+//       "gender": "male",
+//       "name": {
+//         "title": "Mr",
+//         "first": "Elias",
+//         "last": "Christiansen"
+//       },
+//       "location": {
+//         "street": {
+//           "number": 3006,
+//           "name": "Dalvej"
+//         },
+//         "city": "Sundby",
+//         "state": "Syddanmark",
+//         "country": "Denmark",
+//         "postcode": 23799,
+//         "coordinates": {
+//           "latitude": "-44.6656",
+//           "longitude": "-131.3606"
+//         },
+//         "timezone": {
+//           "offset": "-2:00",
+//           "description": "Mid-Atlantic"
+//         }
+//       },
+//       "email": "elias.christiansen@example.com",
+//       "login": {
+//         "uuid": "41541dd2-518e-49a9-987f-c687cde0223c",
+//         "username": "heavywolf823",
+//         "password": "lenny",
+//         "salt": "weI4KOJM",
+//         "md5": "3d582144fd283da5b32a2346a9e49c44",
+//         "sha1": "e9fca9e27a3dd5430980a72e2226d0e6ab629a34",
+//         "sha256": "185190a988586a37e2f741dfcca9f30cd0b1e259da3c8eb21480c7732b2767d7"
+//       },
+//       "dob": {
+//         "date": "1969-02-03T03:49:24.651Z",
+//         "age": 56
+//       },
+//       "registered": {
+//         "date": "2012-12-20T15:12:23.146Z",
+//         "age": 13
+//       },
+//       "phone": "78901127",
+//       "cell": "63801297",
+//       "id": {
+//         "name": "CPR",
+//         "value": "020269-9909"
+//       },
+//       "picture": {
+//         "large": "https://randomuser.me/api/portraits/men/45.jpg",
+//         "medium": "https://randomuser.me/api/portraits/med/men/45.jpg",
+//         "thumbnail": "https://randomuser.me/api/portraits/thumb/men/45.jpg"
+//       },
+//       "nat": "DK"
+//     }
+//   ],
+//   "info": {
+//     "seed": "397100682af9e782",
+//     "results": 1,
+//     "page": 1,
+//     "version": "1.4"
+//   }
+// }
 
